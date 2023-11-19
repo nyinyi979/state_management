e62ae051f1b681ba51c3affd81be31e0f0b02f34
nyinyi979 <nyinyi095062687@gmail.com>
2023-11-20 04:46:13 +0630

    router fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,28 @@ import './App.css'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 import { store } from './slices/store'
-import Home from './page';
-import {Route , BrowserRouter as Router , Routes} from 'react-router-dom'
-import PlayersPage from './Players';
 const persistor = persistStore(store);
+import Home from './page.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import PlayersPage from './Players.tsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/players",
+    element: <PlayersPage />
+  }
+]);
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/players' element={<PlayersPage />} />
-        </Routes>
-        </Router>
+        <RouterProvider router={router}/>
       </PersistGate>
     </Provider>
   );
